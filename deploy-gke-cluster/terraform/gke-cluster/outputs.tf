a246c74dd6056fb752e6d3bd724ef82128ab60d6
Joffrey <joffreybena@gmail.com>
2024-03-01 08:51:04 +0100

    Suppression de commentaires et de sorties inutilisées dans les fichiers outputs.tf

--- deploy-gke-cluster/terraform/gke-cluster/outputs.tf
+++ deploy-gke-cluster/terraform/gke-cluster/outputs.tf
@@ -1,9 +1,3 @@
-# # Certificat CA du cluster GKE
-# output "cluster_ca_certificate" {
-#     value       = google_container_cluster.gke-cluster.master_auth.0.cluster_ca_certificate
-#     description = "Certificat CA du cluster GKE"
-# }
-
 # Output pour l'endpoint externe (public) du cluster
 output "cluster_external_endpoint" {
     value = google_container_cluster.gke-cluster.endpoint
@@ -21,14 +15,3 @@ output "region" {
     value       = google_container_cluster.gke-cluster.location
     description = "Région du cluster GKE"
 }
-
-# # Output pour les adresses IP des nœuds du cluster
-# output "node_ips" {
-#     value       = google_container_cluster.gke-cluster.node_version
-#     description = "Adresses IP des nœuds du cluster GKE"
-# }
-
-# # Output pour l'endpoint interne (privé) du cluster
-# output "cluster_internal_endpoint" {
-#     value = data.google_container_cluster.gke_cluster.internal_ip
-# }
--- deploy-gke-cluster/terraform/outputs.tf
+++ deploy-gke-cluster/terraform/outputs.tf
@@ -7,21 +7,6 @@ output "cluster_external_endpoint" {
     value = module.gke-cluster.cluster_external_endpoint
 }
 
-# # Output pour les adresses IP des nœuds du cluster
-# output "node_ips" {
-#     value = module.gke-cluster.node_ips
-# }
-
-# # Output pour l'endpoint interne (privé) du cluster
-# output "cluster_internal_endpoint" {
-#     value = module.gke-cluster.cluster_internal_endpoint
-# }
-
-# output "cluster_ca_certificate" {
-#     value = module.gke-cluster.cluster_ca_certificate
-#     description = "Certificat CA du cluster GKE"
-# }
-
 output "region" {
     value = module.gke-cluster.region
 }
